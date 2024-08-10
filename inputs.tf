5371827bba98be25aab96f44f1829ae6c2489b09
lichfiet <sortaminty@gmail.com>
2024-08-10 14:18:54 -0600

    swapped .env for tf vars

--- inputs.tf
+++ inputs.tf
@@ -1,7 +1,3 @@
-locals {
-  envs = { for tuple in regexall("(.*)=(.*)", file(".env")) : tuple[0] => sensitive(tuple[1]) }
-}
-
 variable "environment" {
   type = string
   description = "environment of the project"
