608e41da6d7422d1d88d7e22553931cd6bc26033
lichfiet <sortaminty@gmail.com>
2024-11-23 18:08:00 -0700

    add ec2 instance

--- main.tf
+++ main.tf
@@ -94,15 +94,13 @@ module "amplify_app" {
 ## EC2 Instance with K3s
 ##
 
-resource "aws_ec2_instance" "k3s" {
+resource "aws_instance" "k3s" {
     ami = data.aws_ami.ubuntu.id
-    instance_type = "t2.small"
+    instance_type = "t3.small"
     key_name = "trevors-projects"
-    vpc_security_group_ids = [module.vpc.vpc_default_sg_id]
+    vpc_security_group_ids = [module.vpc.public_subnet_sg_id]
     subnet_id = module.vpc.vpc_public_subnets[0]
     associate_public_ip_address = true
-    iam_instance_profile = module.k3s.k3s_profile_name
-    user_data = data.template_cloudinit_config.k3s.rendered
     tags = {
         Name = "k3s-server"
     }
