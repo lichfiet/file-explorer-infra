18b64da63391a53b51770f8516bad42797fb5d65
lichfiet <sortaminty@gmail.com>
2024-11-23 18:24:40 -0700

    bad keypair

--- main.tf
+++ main.tf
@@ -97,7 +97,7 @@ module "amplify_app" {
 resource "aws_instance" "k3s" {
   ami                         = "ami-038bba9a164eb3dc1"
   instance_type               = "t3.small"
-  key_name                    = "trevors-projects"
+  key_name                    = "low-security"
   vpc_security_group_ids      = [module.vpc.vpc_ids.vpc.security_groups.public.id]
   subnet_id                   = module.vpc.vpc_ids.vpc.subnets.public1.id
   associate_public_ip_address = true
